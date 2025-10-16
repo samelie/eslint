df30e831e6aadaa70b55ccdc5083ed6a6b70e0c9
Sam Elie <samradelie@gmail.com>
2025-10-16 11:48:51 -0700

    now

--- build.config.ts
+++ build.config.ts
@@ -1,21 +1,23 @@
 import type { IPackageJson } from "package-json-type";
 import { readFileSync } from "node:fs";
-import { parse } from "node:path";
-import { makeUnbuildConfig } from "@adddog/build-configs/unbuild";
-import { packageUpSync } from "package-up";
 
-const path = require.resolve("@antfu/eslint-config");
-const p = packageUpSync({
-    cwd: parse(path).dir,
-});
-const pkgJson = JSON.parse(readFileSync(p || "", "utf-8")) as IPackageJson;
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineBuildConfig } from "unbuild";
 
-export default makeUnbuildConfig({
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+const pkgJson = JSON.parse(readFileSync(join(__dirname, "package.json") || "", "utf-8")) as IPackageJson;
+
+export default defineBuildConfig({
     entries: ["src/eslint.config"],
-    // Currently there is an issue with one of the eslint plugins & Typescript
+    outDir: "dist",
+    failOnWarn: false,
     declaration: true,
+    clean: true,
     rollup: {
         emitCJS: true,
+        cjsBridge: true,
     },
     externals: [...Object.keys(pkgJson.devDependencies || [])],
 });
